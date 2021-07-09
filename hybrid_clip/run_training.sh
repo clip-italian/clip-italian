abc19000904ac0c1ace3558a9b33019e4db597ea
Giuseppe <giuseppe.attanasio@polito.it>
2021-07-09 09:52:20 +0000

    Bugfix (truncation=True)
    Also:
    - update hparams in the runner bash
    - update gitignore

--- hybrid_clip/run_training.sh
+++ hybrid_clip/run_training.sh
@@ -6,6 +6,7 @@ MODEL_DIR=~/clip-italian
 IMAGE_ENCODER="openai/clip-vit-base-patch32"
 TEXT_ENCODER="dbmdz/bert-base-italian-xxl-uncased"
 
+
 python ${SCRIPT_DIR}/run_hybrid_clip.py \
     --output_dir ${MODEL_DIR} \
     --overwrite_output_dir \
@@ -16,9 +17,9 @@ python ${SCRIPT_DIR}/run_hybrid_clip.py \
     --validation_file="../data/valid_dataset.json" \
     --do_train --do_eval \
     --num_train_epochs="80" --max_seq_length 96 \
-    --per_device_train_batch_size="64" \
+    --per_device_train_batch_size="128" \
     --per_device_eval_batch_size="64" \
-    --learning_rate="5e-5" --warmup_steps="2048" --weight_decay 0.1 \
-    --preprocessing_num_workers 96 \
+    --learning_rate="5e-5" --warmup_steps="526" --weight_decay 0.1 \
+    --preprocessing_num_workers 32 \
     --push_to_hub
 
