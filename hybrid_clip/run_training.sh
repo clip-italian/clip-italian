c35af87aa21dfbd1c6282d1097cbbe31e7ea22e8
Giuseppe Attanasio <giuseppe.attanasio@polito.it>
2021-07-15 12:50:31 +0000

    lower learning rate

--- hybrid_clip/run_training.sh
+++ hybrid_clip/run_training.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SCRIPT_DIR=.
-MODEL_DIR=~/models/from_frozen_backbones_checkpoint_1
+MODEL_DIR=~/models/from_frozen_backbones_checkpoint_2
 
 IMAGE_ENCODER="openai/clip-vit-base-patch32"
 TEXT_ENCODER="dbmdz/bert-base-italian-xxl-uncased"
@@ -17,10 +17,10 @@ python ${SCRIPT_DIR}/run_hybrid_clip.py \
     --num_train_epochs="17" --max_seq_length 96 \
     --per_device_train_batch_size="128" \
     --per_device_eval_batch_size="128" \
-    --learning_rate="0.00007" --warmup_ratio 0.1 --weight_decay 0.0 \
+    --learning_rate="0.00001" --warmup_ratio 0.1 --weight_decay 0.0 \
     --preprocessing_num_workers 32 \
     --log_comet \
-    --exp_name from_frozen_backbones_checkpoint_1 \
+    --exp_name from_frozen_backbones_checkpoint_2 \
     --eval_when 1 \
     --text_model_name_or_path=${TEXT_ENCODER} \
     --vision_model_name_or_path=${IMAGE_ENCODER} \
