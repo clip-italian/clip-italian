9ca42b911bf7b51fbd1c1cb587dffcc6f03a3457
Giuseppe Attanasio <giuseppe.attanasio@polito.it>
2021-07-17 05:52:50 +0000

    final training params

--- hybrid_clip/run_training.sh
+++ hybrid_clip/run_training.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SCRIPT_DIR=.
-MODEL_DIR=~/models/pretraining_on_v4
+MODEL_DIR=~/models/training_v5
 
 IMAGE_ENCODER="openai/clip-vit-base-patch32"
 TEXT_ENCODER="dbmdz/bert-base-italian-xxl-uncased"
@@ -11,22 +11,22 @@ python ${SCRIPT_DIR}/run_hybrid_clip.py \
     --output_dir ${MODEL_DIR} \
     --overwrite_output_dir \
     --tokenizer_name=${TEXT_ENCODER} \
-    --train_file="../data/train_dataset_v4.json" \
-    --validation_file="../data/valid_dataset_v4.json" \
+    --train_file="../data/train_dataset_v5.json" \
+    --validation_file="../data/valid_dataset_v5.json" \
     --do_train --do_eval \
-    --num_train_epochs="50" --max_seq_length 96 \
+    --num_train_epochs="15" --max_seq_length 96 \
     --per_device_train_batch_size="128" \
     --per_device_eval_batch_size="128" \
-    --learning_rate="0.00005" --warmup_ratio 0.1 --weight_decay 0.0 \
+    --learning_rate="0.00001" --warmup_ratio 0.1 --weight_decay 0.0 \
     --preprocessing_num_workers 32 \
     --log_comet \
-    --exp_name pretraining_on_v4 \
-    --eval_when 5 \
+    --exp_name training_v5 \
+    --eval_when 1 \
     --text_model_name_or_path=${TEXT_ENCODER} \
     --vision_model_name_or_path=${IMAGE_ENCODER} \
-    --freeze_backbones
+    --run_from_checkpoint /home/giuseppe/models/pretraining_on_v4/26/
 
 #    --freeze_backbones    # freezes both models except from the reprojections layers
-#    --run_from_checkpoint /home/giuseppe/models/cc_wit_coco_v2_2_resumed/20 \
+#    --run_from_checkpoint /home/giuseppe/models/pretraining_on_v4/26/ \
 #    --push_to_hub
 
